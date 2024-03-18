599704cd6ab7ef73e16d9f4c313c345468379bbb
LowTL <e0957642@u.nus.edu>
2024-03-18 20:03:26 +0800

    Add constants and menu

---
+++ e_mods/e_mods.srcs/sources_1/new/constants.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.03.2024 19:54:44
+// Design Name: 
+// Module Name: constants
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module constants();
+    // Color variables
+parameter COLOR_RED = 16'b11111_000000_00000;
+parameter COLOR_GREEN = 16'b00000_101010_00000;
+parameter COLOR_BLUE = 16'b00000_000000_11111;
+parameter COLOR_YELLOW = 16'b11111_111111_00000;
+parameter COLOR_CYAN = 16'b00000_111111_11111;
+parameter COLOR_MAGENTA = 16'b11111_000000_11111;
+parameter COLOR_ORANGE = 16'b11111_011000_00000;
+parameter COLOR_PURPLE = 16'b01111_000000_11111;
+parameter COLOR_PINK = 16'b11111_010010_10111;
+parameter COLOR_BROWN = 16'b01111_010100_00000;
+parameter COLOR_WHITE = 16'b11111_111111_11111;
+parameter COLOR_GRAY = 16'b01010_010101_01010;
+parameter COLOR_LIGHT_BLUE = 16'b01111_011111_11111;
+parameter COLOR_LIGHT_GREEN = 16'b01000_111111_01000;
+parameter COLOR_LIGHT_YELLOW = 16'b11111_111111_01000;
+parameter COLOR_LIGHT_PURPLE = 16'b10111_010000_10111;
+parameter COLOR_LIGHT_GRAY = 16'b10101_101010_10101;
+parameter COLOR_DARK_GRAY = 16'b00101_001010_00101;
+parameter COLOR_BLACK = 16'd0;
+
+endmodule
---
+++ e_mods/e_mods.srcs/sources_1/new/menu/menuCode.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.03.2024 19:46:50
+// Design Name: 
+// Module Name: menuCode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module menuCode(
+        input clk, reset,
+        input btnC, btnR, btnL, 
+        input [15:0] sw,
+        input [12:0] oled_pixel_index, output [15:0] oled_pixel_data
+    );
+    
+    constants constant();
+    
+    wire [12:0] pixel_index = oled_pixel_index;
+    reg [15:0] pixel_data;
+    assign oled_pixel_data = pixel_data;
+    
+    always @ (*) begin
+        pixel_data <= constant.COLOR_WHITE;
+    end
+    
+    
+endmodule
